04ea80c066889fe052128e83e0d61acfccc0d94e
me2001ru <mehsum.rupani@yh.nackademin.se>
2021-09-09 17:47:37 +0200

    changed yml file and other files updated

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,3 +16,8 @@ def test_sample_multiple_words():
     assert words[0] in l
     assert words[1] in l
     assert words[0] is not words[1]
+
+
+def test_generate_buzz_of_at_least_five_words():
+    phrase = main.generate_buzz()
+    assert len(phrase.split()) >= 5
