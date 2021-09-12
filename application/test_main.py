71e1eeff777f30f75c5f12435f8ba44516a2a289
me2001ru <mehsum.rupani@yh.nackademin.se>
2021-09-12 22:12:49 +0200

    test added in test_main for yml reaction

--- application/test_main.py
+++ application/test_main.py
@@ -1,1 +1,12 @@
 # empty, add tests here.
+import sqlite3
+import pytest
+import init_db
+
+
+# def test_hello(client):
+#     response = client.get("/hello")
+#     assert response.data == b"Hello, World!"
+
+def test_reversed():
+    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
