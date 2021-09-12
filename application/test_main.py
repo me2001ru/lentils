69091ebd6843bdfb45f4307fe372fa57204ec33a
me2001ru <mehsum.rupani@yh.nackademin.se>
2021-09-12 22:35:23 +0200

    @pytest added

--- application/test_main.py
+++ application/test_main.py
@@ -1,7 +1,6 @@
 # empty, add tests here.
 import sqlite3
 import pytest
-import init_db
 
 
 # def test_hello(client):
@@ -10,3 +9,11 @@ import init_db
 
 def test_reversed():
     assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
+
+
+@pytest.fixture
+def session():
+    connection = sqlite3.connect(':memory:')
+    db_session = connection.cursor()
+    yield db_session
+    connection.close()
